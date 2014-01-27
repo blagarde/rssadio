e28b786013c138246e7c8c8c321329d4f2148fc7
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2014-01-27 18:55:43 +1100

    ADD: Recommender app draft

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 FEEDS = os.path.join(HERE, 'feeds.txt')
+MODEL = os.path.join(HERE, 'model.pkl')
+GOOD, BAD = 'GOOD', 'BAD'
--- learn.py
+++ learn.py
@@ -1,7 +1,12 @@
+import os
 import re
+import numpy as np
 from string import punctuation
 from sklearn.feature_extraction.text import HashingVectorizer
-from bing import xbinglinks
+from sklearn import linear_model
+from feeds import stories
+from cPickle import load, dump
+from config import MODEL, GOOD, BAD
 
 
 WHITESPACE_RE = re.compile('\s+', flags=re.UNICODE)
@@ -13,14 +18,48 @@ def preprocess(doc):
     return WHITESPACE_RE.sub(' ', doc).strip(' ')
 
 
-hv = HashingVectorizer()
+class App(object):
+    def __init__(self):
+        self.hv = HashingVectorizer(norm=None, non_negative=True)
+        if os.path.isfile(MODEL):
+            self.clf = load(MODEL)
+        else:
+            self.clf = linear_model.SGDClassifier(warm_start=True)
+            zeros = self.vector('seed')
+            self.clf.partial_fit(zeros, np.unique([GOOD]), classes=(GOOD, BAD))
 
-X = []
-y = []
+    def feed(self):
+        for doc in stories:
+            self.pred_X = self.vector(doc)
+            self.pred_y = self.clf.predict(self.pred_X)
+            if self.pred_y == GOOD:
+                yield doc
 
-topics = ['technology', 'sports']
-for search_query in topics:
-    y += [search_query]
-    X += [preprocess(doc) for doc in xbinglinks(search_query, limit=10)]
+    def save_model(self):
+        dump(self.clf, MODEL)
 
-hv.transform(X)
+    def train(self, doc, y):
+        X = self.vector(doc)
+        self.clf.partial_fit(X, [y], classes=(GOOD, BAD))
+
+    def vector(self, doc):
+        clean = preprocess(doc)
+        return self.hv.transform([clean])
+
+    def score(self, y):
+        print self.clf.score(self.pred_X, [y])
+
+
+if __name__ == "__main__":
+    app = App()
+    for doc in app.feed():
+        print doc
+        label_dct = {'G': GOOD, 'B': BAD, 'S': 'skip'}
+        value = ''
+        while value.upper() not in label_dct:
+            value = raw_input("What do you think about the above?\n(G)ood | (B)ad | (S)kip\n").upper()
+        if value == 'S':
+            continue
+        label = label_dct[value]
+        app.score(label)
+        app.train(doc, label)
